770ba97751284d969e6913c3ee7483625176ad89
jayuRa <jayupatel7778@gmail.com>
2022-11-15 15:31:01 +0530

    second1

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse,RedirectResponse
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates=Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.api_route("/", response_class=HTMLResponse,methods=['GET','POST'])
+async def showData(request: Request):
+     
+    return templates.TemplateResponse('registration.html', {"request": request})
 
 
 @app.get("/items/{item_id}")    
